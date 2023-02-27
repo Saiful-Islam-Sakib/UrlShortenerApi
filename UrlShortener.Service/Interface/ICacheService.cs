ccfe6e6fe2c909d43e77e3825537d94092323006
Saiful-Islam-Sakib <saifulislam.sakib96@gmail.com>
2023-02-28 01:48:33 +0600

    interface for caching

---
+++ UrlShortener.Service/Interface/ICacheService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlShortener.ApiService.Interface
+{
+	public interface ICacheService
+	{
+		T GetData<T>(string key) where T : class;
+		bool SetData<T>(string key, T value, DateTime expirationTime) where T : class;
+		T RemoveData<T>(string key) where T : class;
+	}
+}
---
+++ UrlShortener.Service/Service/CacheService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrlShortener.ApiService.Interface;
+
+namespace UrlShortener.ApiService.Service
+{
+	public class CacheService : ICacheService
+	{
+		public T GetData<T>(string key) where T : class
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool SetData<T>(string key, T value, DateTime expirationTime) where T : class
+		{
+			throw new NotImplementedException();
+		}
+
+		public T RemoveData<T>(string key) where T : class
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
